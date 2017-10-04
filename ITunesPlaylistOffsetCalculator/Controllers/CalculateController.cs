cf6e3f92c80fea680e49d951568d656bebc528ae
Cyberboss <Cyberboss@users.noreply.github.com>
2017-10-04 14:47:49 -0400

    With the COM wew

--- ITunesPlaylistOffsetCalculator/Controllers/CalculateController.cs
+++ ITunesPlaylistOffsetCalculator/Controllers/CalculateController.cs
@@ -1,10 +1,16 @@
-﻿using System.Web.Http;
+﻿using iTunesLib;
+using System.Web.Http;
 
 namespace ITunesPlaylistOffsetCalculator.Controllers
 {
 	public class CalculatorResult
 	{
-
+		int PlaylistID;
+		public CalculatorResult()
+		{
+			var res = new iTunesApp();
+			PlaylistID = res.CurrentPlaylist.playlistID;
+		}
 	}
 	public class CalculateController : ApiController
 	{
